075224fa9981849f18d4c6310b5cf4291f2a3366
parmeet2 <parmeet@damacloid.com>
2022-05-16 22:04:14 +0530

    Changed PORT for heroku

--- index.js
+++ index.js
@@ -257,6 +257,6 @@ app.post("/generatePassword", async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running on port " + PORT);
 });
